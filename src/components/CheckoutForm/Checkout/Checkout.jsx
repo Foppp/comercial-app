707f24a18ba63dffa4ba2d563e64885df13a68c7
Yura <tt@list.ru>
2021-08-16 13:35:31 +0300

    payment methods added

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -9,7 +9,7 @@ import PaymentForm from './PaymentForm';
 
 const steps = ['Shipping adress', 'Payment details'];
 
-const Checkout = ({ cart }) => {
+const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
   const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
   const [shippingData, setShippingData] = useState([]);
@@ -42,8 +42,17 @@ const Checkout = ({ cart }) => {
   );
 
   const Form = () => activeStep === 0
-    ? <AddressForm checkoutToken={checkoutToken} next={next} />
-    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} />
+    ? <AddressForm
+      checkoutToken={checkoutToken}
+      next={next}
+    />
+    : <PaymentForm
+      shippingData={shippingData}
+      checkoutToken={checkoutToken}
+      backStep={backStep}
+      nextStep={nextStep}
+      onCaptureCheckout={onCaptureCheckout}
+    />
   
   return (
     <>
