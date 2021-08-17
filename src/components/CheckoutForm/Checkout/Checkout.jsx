69d22690b19c03f936a76ec215d7044310f449ed
Yura <tt@list.ru>
2021-08-17 12:42:20 +0300

    purchase confirmation message

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -22,7 +22,7 @@ const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
         const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' })
         setCheckoutToken(token);
       } catch (e) {
-        throw e;
+        console.log(e);
       }
     }
     generateToken();
@@ -39,9 +39,11 @@ const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
   const Confirmation = () => order.customer ? (
     <>
       <div>
-        <Typography variant="h5">Thank you for your purchase, firstname lastname</Typography>
+        <Typography variant="h5">
+          Thank you for your purchase, {order.customer.firstname} {order.customer.lastname}
+        </Typography>
         <Divider className={classes.divider} />
-        <Typography variant="subtitle2">Order ref: ref</Typography>
+        <Typography variant="subtitle2">Order ref: {order.customer_reference}</Typography>
       </div>
       <br />
       <Button component={Link} to="/" variant="outlined" type="button">Back to Home</Button>
