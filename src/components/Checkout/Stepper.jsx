460ec622ce7114f8ab1ad42eea7ff7eb6afb9a8b
Yura <tt@list.ru>
2021-09-09 11:32:51 +0300

    progress line

--- src/components/Checkout/Stepper.jsx
+++ src/components/Checkout/Stepper.jsx
@@ -3,20 +3,32 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const Stepper = () => {
   const steps = useSelector((state) => state.checkoutInfoReducer.steps);
-
+  const currentStepId = useSelector((state) => state.checkoutInfoReducer.currentStepId);
+  const currentStep = steps.find((step) => step.id === currentStepId);
   return (
-    <div class='container mt-2'>
-      <div class='row text-center d-flex justify-content-evently'>
+    <div className='container mt-2'>
+      <div className='progress mb-3' style={{ height: '60px' }}>
+        <div
+          className='progress-bar bg-success text-wrap'
+          role='progressbar'
+          style={{ width: `${(currentStepId) * 25}%` }}
+          aria-valuenow='25'
+          aria-valuemin='0'
+          aria-valuemax='100'
+        ><span className="fs-6 text-dark">{currentStep.name}</span></div>
+      </div>
+      <div className='row text-center d-flex justify-content-evently'>
+
         {steps.map((step) => (
-          <div key={step.id} class='col'>
+          <div key={step.id} className='col'>
             <div>
-              {!step.finished ? (
+              {/* {!step.finished ? (
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   width='30'
                   height='30'
                   fill='currentColor'
-                  class='bi bi-check-circle'
+                  className='bi bi-check-circle'
                   viewBox='0 0 16 16'
                 >
                   <path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z' />
@@ -28,14 +40,14 @@ const Stepper = () => {
                   width='30'
                   height='30'
                   fill='green'
-                  class='bi bi-check-circle-fill'
+                  className='bi bi-check-circle-fill'
                   viewBox='0 0 16 16'
                 >
                   <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z' />
                 </svg>
-              )}
+              )} */}
             </div>
-            <small className="">{step.name}</small>
+            {/* <small className=''>{step.name}</small> */}
           </div>
         ))}
       </div>
