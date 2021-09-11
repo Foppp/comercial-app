404c8854eda07fa2315b4645f3861fff76446db4
Yura <tt@list.ru>
2021-09-11 10:12:30 +0300

    added cart quick view fro modal window

---
+++ src/components/Cart/CartQuickView.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { emptyCart } from '../../redux/cart/asyncThunk';
+import CartItem from './CartItem';
+import Spinner from '../Spinner/Spinner';
+
+const CartQuickView = ({ onClose }) => {
+  const cart = useSelector((state) => state.cartInfoReducer.cart);
+  const dispatch = useDispatch();
+
+  const renderEmptyCart = () => (
+    <div className='container text-center mt-5'>
+      <h1>Your cart is empty!</h1>
+      <h3>
+        <Link to='/products' type='button' className='btn btn-secondary'>
+          Start shopping now
+        </Link>
+      </h3>
+    </div>
+  );
+
+  if (!cart.line_items)
+    return (
+      <div className='cart-container d-flex justify-content-center'>
+        <Spinner />
+      </div>
+    );
+
+  const renderCart = () => (
+    <>
+      {cart.line_items.map((lineItem) => (
+        <CartItem key={lineItem.id} item={lineItem} />
+      ))}
+      <div className='d-flex flex-row-reverse text-center row mt-3'>
+        <div className='col-md-2'>
+          <h5 className=''>Total: {cart.subtotal.formatted_with_symbol}</h5>
+        </div>
+      </div>
+      <div className='shopping-cart-footer mt-3'>
+        <div className='column float-start'>
+          <Link
+            to='/products'
+            className='btn btn-outline-secondary rounded-pill'
+            onClick={() => onClose()}
+          >
+            Back to Shopping
+          </Link>
+        </div>
+        <div className='column float-end'>
+          <Link
+            to='/cart'
+            className='btn btn-warning mx-3 rounded-pill'
+            onClick={() => onClose()}
+          >
+            Go to cart
+          </Link>
+          <Link
+            to='/checkout'
+            className='btn btn-info rounded-pill'
+            onClick={() => onClose()}
+          >
+            Checkout
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+
+  return (
+    <div className=''>
+      {!cart.line_items.length ? renderEmptyCart() : renderCart()}
+    </div>
+  );
+};
+
+export default CartQuickView;
