082f4ae96e2a601d52ad9e5258ebedaf13f582e1
Yura <tt@list.ru>
2021-09-11 13:43:48 +0300

    search and cart layout fixed

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -71,26 +71,22 @@ const Navbar = () => {
             })}
           </ul>
           <div className="d-flex input-group-sm">
-            <input
-              className="form-control rounded me-2"
-              type="text"
-              placeholder="Search"
-              aria-label="Search"
-              onChange={(e) => console.log(e.target.value)}
-            />
+          </div>
+          <div className="d-sm-flex">
+          <button className="btn search" type="button">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="26"
-              height="26"
+              width="25"
+              height="25"
               fill="currentColor"
-              className="bi bi-search mt-2"
+              className="bi bi-search"
               viewBox="0 0 16 16"
             >
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
             </svg>
-          </div>
-          <div className="ms-5 mt-3"></div>
-          <button type="button" className="btn btn-outline-dark rounded-pill ms-3" onClick={() => dispatch(setModalOpen())}>
+            </button>
+            <div className="ms-5 mt-3"></div>
+          <button type="button" className="btn cart btn-outline-dark rounded-pill" onClick={() => dispatch(setModalOpen())}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -106,10 +102,10 @@ const Navbar = () => {
               {totalItems ?? 0}
             </span>
           </button>
+          </div>
         </div>
       </div>
       <ModalWindow />
-
     </nav>
   );
 };
