388ee880747e1615b8b8120ba16ace4e890fe9b0
Yura <tt@list.ru>
2021-09-12 16:15:19 +0300

    removed filter list array

--- src/redux/filter/filter.js
+++ src/redux/filter/filter.js
@@ -1,14 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-import _ from 'lodash';
 
 export const filterProductsInfo = createSlice({
   name: 'filter',
   initialState: {
-    filters: [
-      { id: _.uniqueId(), name: 'Price Range', active: true },
-      { id: _.uniqueId(), name: 'Manufacturer', active: false },
-      { id: _.uniqueId(), name: 'Keys Quantity', active: false },
-    ],
     filterBy: {
       price: {
         min: null,
