66275a282c05d577a6f383afd61c40e8446b3e8c
Yura <tt@list.ru>
2021-09-13 17:10:07 +0300

    removed defaultSortOption state

--- src/redux/sort/sort.js
+++ src/redux/sort/sort.js
@@ -3,7 +3,6 @@ import { createSlice } from "@reduxjs/toolkit";
 export const sortProductsInfo = createSlice({
   name: 'sort',
   initialState: {
-    defaultSortOption: 'aaaaa',
     sortOption: null,
   },
   reducers: {
