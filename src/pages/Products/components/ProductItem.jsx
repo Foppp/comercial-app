318fbfe6ded9669faad57842788ca902f7f25c8b
Yura <tt@list.ru>
2021-09-15 00:32:52 +0300

    card item header added, d-flex

--- src/pages/Products/components/ProductItem.jsx
+++ src/pages/Products/components/ProductItem.jsx
@@ -8,25 +8,30 @@ const ProductItem = ({ product }) => {
 
   return (
     <div className='col mb-5'>
-      <div className='card product-card h-100'>
-        <Link
-          to={`products/${product.permalink}`}
-          className='product-info text-decoration-none'
-        >
+      <div className='card product-card h-100 d-flex flex-column justify-content-between'>
+        <div className='card-header'>
           <img
             className='card-img-top'
             src={product.media.source}
             alt={`${product.name}`}
           />
-          <div className='card-body p-1 text-black'>
-            <div className='text-center mt-3'>
-              <p className='card-title'>{product.name}</p>
-              <h5 className='card-text mt-auto'>
-                {product.price.formatted_with_symbol}
-              </h5>
+        </div>
+        <div className='card-body p-1 text-black d-flex flex-column justify-content-between'>
+          <Link
+            to={`products/${product.permalink}`}
+            className='product-info text-decoration-none'
+          >
+            <div>
+              <p className='card-title mt-2 text-dark'>{product.name}</p>
             </div>
+          </Link>
+
+          <div>
+            <h5 className='card-text mt-auto'>
+              {product.price.formatted_with_symbol}
+            </h5>
           </div>
-        </Link>
+        </div>
         <div className='card-footer border-top-0 bg-transparent text-center'>
           <button
             className='btn btn-outline-dark rounded-pill mt-auto'
