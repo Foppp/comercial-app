6448e2683fdce013cfdca77346f005364938dd01
Yura <tt@list.ru>
2021-10-08 14:13:47 +0300

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="footer mt-auto py-3 bg-dark">
-      <div className="container-footer">
-        <p className="m-0 text-center text-white">Copyright &copy; SynthMaster Shop 2021</p>
-      </div>
+    <footer className="container-footer mt-auto py-3 bg-dark'">
+      <ul className='nav justify-content-center pb-2 mb-2 border-top'>
+        <li className='nav-item'>
+          <Link to='/' className='nav-link px-2 text-muted'>
+            Home
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link to='/products' className='nav-link px-2 text-muted'>
+            Products
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link to='/contact' className='nav-link px-2 text-muted'>
+            Contact
+          </Link>
+        </li>
+      </ul>
+      <p className='text-center text-muted'>© 2021 SynthMaster, Inc</p>
     </footer>
-  )
+  );
 }
 
 export default Footer;
